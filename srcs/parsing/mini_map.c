0fe078429f4fe18b5acfbba4c22f2e996ecb1b84
SolDavid_76 <david.janusz75@gmail.com>
2023-08-16 11:14:17 +0200

    merge

--- srcs/parsing/mini_map.c
+++ srcs/parsing/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 12:41:20 by djanusz           #+#    #+#             */
-/*   Updated: 2023/08/11 13:18:40 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/08/16 11:13:43 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,18 @@ void	mini_map(t_win *win, t_player *player)
 	int	j;
 
 	i = 0;
-	while (i < 150)
+	while (i < 5)
 	{
 		j = 0;
-		while (j < 150)
+		while (j < 5)
 		{
-			if (win->data->map[(int)player->pos_x + ((i / 30) - 2)][(int)player->pos_y + ((j / 30) - 2)] == '1')
-				mini_map_aux(&win->mini_map, i, j, 0xff0000);
+			if (win->data->map[(int)player->pos_x + (i - 2)][(int)player->pos_y + (j - 2)] == '1')
+				mini_map_aux(&win->mini_map, i * 30, j * 30, 0x0fffaf);
 			else
-				mini_map_aux(&win->mini_map, i, j, 0xffffff);
-			// if (win->data->map[(int)player->pos_x + ((i / 30) - 2)][(int)player->pos_y + ((j / 30) - 2)] == '0')
-			j += 30;
+				mini_map_aux(&win->mini_map, i * 30, j * 30, 0xffffff);
+			j++;
 		}
-		i += 30;
+		i++;
 	}
 	mlx_put_image_to_window(win->mlx, win->ptr, win->mini_map.ptr, 10, 10);
 }
